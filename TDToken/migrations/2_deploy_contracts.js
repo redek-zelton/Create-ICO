b7ab6aed5507243daeb6c8f2fff3ca4d07e4c1e6
redek-zelton <64832190+redek-zelton@users.noreply.github.com>
2021-03-15 00:46:52 +0100

    Update 2_deploy_contracts.js

--- TDToken/migrations/2_deploy_contracts.js
+++ TDToken/migrations/2_deploy_contracts.js
@@ -1,6 +1,6 @@
-const TDCOIN = artifacts.require("./TDToken.sol");
+var TDCOIN = artifacts.require("TDToken");
 
 module.exports = function (deployer) {
   // deployer.deploy(#Nom, #TotalSupply, #Address Admin)
-  deployer.deploy(TDCOIN,100000000000000, '0xa58155a23f5628c3690b4E07d7D9AaCF214BEDbA');
+  deployer.deploy(TDCOIN,100000000000000, '0x9d40750332cE4f6e6c49D72658076A1f02Fb53f8');
 };
